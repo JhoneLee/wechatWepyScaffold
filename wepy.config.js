a527cf1bcd95d54a139605ba8a384584e4143a0d
JhoneLee <liyunjiao2048@gmail.com>
2018-07-03 22:13:51 +0800

    modify

---
+++ wepy.config.js
@@ -0,0 +1,82 @@
+const path = require('path');
+var prod = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  wpyExt: '.wpy',
+  cliLogs: true,
+  resolve: {
+    alias: {
+      Service: path.join(__dirname, 'src/service'),
+      Utils: path.join(__dirname,'src/utils'),
+      Mixins: path.join(__dirname,'src/mixins')
+      // app: path.join(__dirname, 'src/app'),
+    },
+    aliasFields: ['wepy', 'weapp'],
+    modules: ['node_modules']
+  },
+  compilers: {
+    pug: {
+      pretty: true
+    },
+    less: {
+      compress: true
+    },
+    sass: {
+      outputStyle: 'compressed'
+    },
+    babel: {
+      sourceMap: true,
+      presets: [
+        'env'
+      ],
+      plugins: [
+        'transform-class-properties',
+        'transform-decorators-legacy',
+        'transform-export-extensions',
+        'syntax-export-extensions',
+        'transform-object-rest-spread'
+      ]
+    }
+  },
+  plugins: {
+  },
+  appConfig: {
+    noPromiseAPI: ['createSelectorQuery']
+  }
+};
+
+if (prod) {
+  delete module.exports.compilers.babel.sourcesMap;
+
+  // 压缩sass
+  module.exports.compilers['sass'] = {outputStyle: 'compressed'};
+
+  // 压缩less
+  module.exports.compilers['less'] = {compress: true};
+
+  // 压缩js
+  module.exports.plugins = {
+    uglifyjs: {
+      filter: /\.js$/,
+      config: {
+      }
+    },
+    imagemin: {
+      filter: /\.(jpg|png|jpeg)$/,
+      config: {
+        jpg: {
+          quality: 80
+        },
+        png: {
+          quality: 80
+        }
+      }
+    },
+    autoprefixer: {
+      filter: /\.(wxss|css)$/,
+      config: {
+        browsers: ['last 11 iOS versions']
+      }
+    }
+  };
+}
